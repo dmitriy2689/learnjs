6e70c6cf1d1dd76ff063a37bf486b56319d26168
maria-p1989 <pogorila.mariya.o@gmail.com>
2020-08-04 10:11:19 +0300

    adding first text

---
+++ test.js
@@ -0,0 +1,27 @@
+//alert('Hello');
+
+//variables
+/*let admin;
+let name = 'Jhon';
+
+admin = name;
+
+alert(admin); */
+
+//type-conversions
+"" + 1 + 0  //10  (приводим к строке)
+"" - 1 + 0  //-1
+true + false  //false    1 ?????
+6 / "3" //undefined   2?????
+"2" * "3"  //6
+4 + 5 + "px"  //45px    9px потому что строка в конце
+"$" + 4 + 5  //$45
+"4" - 2  // undefined    2????
+"4px" - 2  //NaN??
+7 / 0  //NaN         Infinity????
+"  -9  " + 5 //-95
+"  -9  " - 5  //-9-5
+null + 1 //undefined
+undefined + 1  //undefined
+" \t \n" - 2  //\t\n-2
+
