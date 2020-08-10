a5e47330f16affc7a1187738e35dfb363c070ad1
natali <n.shutenko@cleack.com>
2020-08-10 08:01:24 +0300

    comprasions

---
+++ comparison/index.js
@@ -0,0 +1,7 @@
+console.log('5 > 4', 5 > 4); //true
+console.log("ананас" > "яблоко"); // false символ "а" меньше чем "я"
+console.log("2" > "12"); //false не правильно (обьяснить !!!)
+console.log(undefined == null); //true
+console.log(undefined === null); // false
+console.log(null == "\n0\n"); // false
+console.log(null === +"\n0\n"); // false
